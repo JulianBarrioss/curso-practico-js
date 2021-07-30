036249046b6defb7d0aaaa83f8e44fed39084a5a
Julian Barrios <julianbarrios0821@gmail.com>
2021-07-30 17:57:27 -0600

    unfinished project

--- average/average.js
+++ average/average.js
@@ -1,3 +1,5 @@
+//average
+
 function calculateMean(list){
     const listSum = list.reduce(
         function (accumulatedValue = 0, newElement){
@@ -6,4 +8,31 @@ function calculateMean(list){
     );
     const averageList = listSum / list.length;
     return averageList;
+}
+
+//medium
+
+const list1 = [100,200,500, 4000000];
+
+const halfList1 = parseInt(list1.length / 2);
+
+function pair(number){
+    if (number % 2 === 0) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let medium;
+
+if (pair(list1.length)) {
+    const element1 = list1[halfList1];
+    const element2 = list1[halfList1 + 1];
+
+    const averageElement1And2 = calculateMean([element1, element2])
+}
+else{
+    medium = list1[halfList1];
+
 }
